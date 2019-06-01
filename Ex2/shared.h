e6de1adfad2879389482bf17c8473eb8cf5b0286
netanelf <netanel.fayoumi@mail.huji.ac.il>
2019-06-01 15:51:36 +0300

    test

--- Ex2/shared.h
+++ Ex2/shared.h
@@ -49,6 +49,10 @@ using namespace std::chrono;
 #define RESULTS_HEADER "Msg size:\t#Sockets:\t#Threads:\tTotal latency:\tTotal throughput:\tTotal packet rate:\n"
 #define RESULTS_FORMAT "%ld\t%s\t%u\t%d\t%f\t%s\t%.3f\t%s\t%f\t%s\n"
 
+#define IB_PACKET_PER_CYCLE 100
+#define IB_NUM_OF_CYCLE 1000
+
+
 #define DEBUG true
 
 #endif //COMM_NETS_SHARED_H
